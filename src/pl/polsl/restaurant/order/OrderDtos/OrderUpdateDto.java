0f132e6cde4aa7b6521c6008d361b7f0673b3cb4
DanielCogiel <dcogiel.work@gmail.com>
2023-08-29 21:46:32 +0200

    Drobna poprawka w OrderUpdateDto

--- src/pl/polsl/restaurant/order/OrderDtos/OrderUpdateDto.java
+++ src/pl/polsl/restaurant/order/OrderDtos/OrderUpdateDto.java
@@ -3,9 +3,6 @@ package pl.polsl.restaurant.order.OrderDtos;
 import java.util.ArrayList;
 import java.util.List;
 
-import pl.polsl.restaurant.customer.Customer;
-import pl.polsl.restaurant.meal.Meal;
-
 public class OrderUpdateDto {
 	private List<Integer> meals = new ArrayList<Integer>();
 	private int customer;
